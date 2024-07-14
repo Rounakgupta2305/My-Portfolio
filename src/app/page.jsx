84df7076be08098b8a9a1a4f50bbf4890a2d593f
Rounak Gupta <Rounak.1si21ec080@gmail.com>
2024-07-15 03:25:04 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,16 +3,11 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
-/*
-<div className="h-1/2 lg:h-full lg:w-1/2 relative">
-  <Image src="/hero.png" alt="" fill className="object-contain" /> 
-</div> 
-
-*/
 const Homepage = () => {
   const handleResumeClick = () => {
     window.location.href = "https://drive.google.com/drive/folders/19WX3kEaVnSP_OfJqk8fp2XiDIkE2mv3P?usp=sharing";
   };
+  
   return (
     <motion.div
       className="h-full"
@@ -26,29 +21,28 @@ const Homepage = () => {
           <Image src="/him.png" alt="" fill className="object-contain" />
         </div>
 
-
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-4 justify-center">
           {/* TITLE */}
           <Image
-              src="/hero.png"
-              alt=""
-              width={112}
-              height={112}
-              className="w-28 h-28 rounded-full object-contain"
-            />
+            src="/hero.png"
+            alt=""
+            width={112}
+            height={112}
+            className="w-28 h-28 rounded-full object-contain"
+          />
           <h1 className="text-4xl md:text-7xl font-bold">
-           Hey there,
+            Hey there,
           </h1>
           <h1 className="text-4xl md:text-7xl font-bold">
-           I'm Rounak
+            I&apos;m Rounak
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-          A full-stack developer with a passion for creating seamless and engaging user experiences, I excel in crafting elegant code and exploring complex algorithms. I am always eager to explore and learn new things, continually expanding my knowledge and skills. My expertise spans both front-end and back-end technologies, allowing me to deliver comprehensive solutions. I thrive in dynamic environments, where I can collaborate with others to innovate and drive projects to success.
+            A full-stack developer with a passion for creating seamless and engaging user experiences, I excel in crafting elegant code and exploring complex algorithms. I am always eager to explore and learn new things, continually expanding my knowledge and skills. My expertise spans both front-end and back-end technologies, allowing me to deliver comprehensive solutions. I thrive in dynamic environments, where I can collaborate with others to innovate and drive projects to success.
           </p>
           {/* BUTTONS */}
-          <div className="w-full ">
+          <div className="w-full">
             <button className="p-4 flex gap-3 rounded-lg ring-1 ring-black bg-black text-white" onClick={handleResumeClick}>
               <Image src="/download.png" alt="" width={24} height={24} />
               Resume
